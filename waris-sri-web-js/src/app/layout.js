843fbd5249ad8b801361da034c00f6ef2d994919
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-09-02 21:14:50 +0700

    Finished

--- waris-sri-web-js/src/app/layout.js
+++ waris-sri-web-js/src/app/layout.js
@@ -1,7 +1,7 @@
-import { Source_Sans_3 } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const source_sans_3 = Source_Sans_3({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "MUICT DevClub Form",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={source_sans_3.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
