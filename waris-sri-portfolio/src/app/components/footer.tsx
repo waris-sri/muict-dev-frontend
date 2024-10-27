05c25b3a56efd6bd8d5c7dc4c7d559f50e9095cb
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-10-27 22:27:20 +0700

    Modified footer and sm elements

--- waris-sri-portfolio/src/app/components/footer.tsx
+++ waris-sri-portfolio/src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-white py-4 px-5 bg-black bg-opacity-[0.5] backdrop-blur-md fixed bottom-0 left-0 right-0">
+    <footer className="text-white text-sm py-2 bg-black bg-opacity-[0.5] backdrop-blur-md fixed bottom-0 left-0 right-0">
       <div className="max-w-7xl mx-auto px-4 text-center">
         © 2024 Waris Sripatoomrak. All rights reserved.
       </div>
--- waris-sri-portfolio/src/app/page.tsx
+++ waris-sri-portfolio/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="min-h-screen flex flex-col pt-[72px]">
         <main className="flex-grow flex flex-col items-center p-[5rem] z-10 bg-transparent">
           <div className="h-auto flex flex-col items-center">
-            <div className="font-bold text-5xl md:text-[3rem] lg:text-6xl z-50 text-center">
+            <div className="font-bold text-4xl md:text-[3rem] lg:text-6xl z-50 text-center">
               <div className="flex flex-col items-center overflow-hidden">
                 <h1 className="animate-name text-yellow-400">
                   Waris Sripatoomrak
@@ -40,7 +40,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <p className="mt-[4rem] change-color text-lg md:text-xl lg:text-2xl lg:mt-[5rem] z-50 text-center">
+          <p className="mt-[4rem] change-color text-md md:text-xl lg:text-2xl lg:mt-[5rem] z-50 text-center">
             A first-year undergraduate pursuing B.Sc. in ICT
           </p>
         </main>
