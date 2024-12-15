26e8ce3a7664c540eb1d0a7a4c4b57d75e0e51df
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2024-12-15 20:14:18 +0700

    + Tried to fix Vercel errors

--- FW-Training-Quiz/src/app/acknowledgement/page.js
+++ FW-Training-Quiz/src/app/acknowledgement/page.js
@@ -1,11 +1,13 @@
 export default function acknowledgement() {
-    return (
-        <>
-            <div className='flex flex-col justify-items-center justify-self-center my-36 items-center content-center justify-center text-[#1A4789] text-xl'>
-                <h1 className="mb-3 font-extrabold ">Acknowledgement</h1>
-                <h1 className="mt-3 font-bold ">This website has been developed by</h1>
-                <h1 className="font-regular hover:text-[#AD841F]">Poonyawatt "poon" Klumnaim - Faculty of ICT</h1>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex flex-col justify-items-center justify-self-center my-36 items-center content-center justify-center text-[#1A4789] text-xl">
+        <h1 className="mb-3 font-extrabold ">Acknowledgement</h1>
+        <h1 className="mt-3 font-bold ">This website has been developed by</h1>
+        <h1 className="font-regular hover:text-[#AD841F]">
+          Poonyawatt &quot;poon&quot; Klumnaim - Faculty of ICT
+        </h1>
+      </div>
+    </>
+  );
 }
--- FW-Training-Quiz/src/app/interviewees/[slug]/page.tsx
+++ FW-Training-Quiz/src/app/interviewees/[slug]/page.tsx
@@ -1,94 +1,143 @@
-import React from "react";
-import useState from "react";
+// Server-side
 
-import {
-  Button,
-  Center,
-  Link,
-  SimpleGrid
-} from '@chakra-ui/react'
-import Image from 'next/image';
+import React from "react";
+import { Center, Link, SimpleGrid } from "@chakra-ui/react";
+import Image from "next/image";
+
+const teams = [
+  {
+    ID: "FW",
+    name: "FRONTEND WEB",
+    imgSrc: "/fwLogo.svg",
+    color: "text-[#00FF66]",
+  },
+  {
+    ID: "FA",
+    name: "FRONTEND APP",
+    imgSrc: "/faLogo.svg",
+    color: "text-[#006AFF]",
+  },
+  {
+    ID: "BN",
+    name: "BACKEND",
+    imgSrc: "/bnLogo.svg",
+    color: "text-[#FF5656]",
+  },
+  {
+    ID: "DA",
+    name: "DESIGN & ART",
+    imgSrc: "/daLogo.svg",
+    color: "text-[#FA00FF]",
+  },
+  {
+    ID: "QA",
+    name: "Quality Assurance",
+    imgSrc: "/qaLogo.svg",
+    color: "text-[#FFC700]",
+  },
+];
+
+const interview = [
+  {
+    ID: "6488a",
+    name: "Thanapat Nonpassopon",
+    form: "https://calendly.com/muictdevclub/muict-dev-club-member-interview-group-i-clone",
+    type: [
+      {
+        ID: "DA",
+        interID: "00",
+        date: "October 31, 2023",
+        time: "00.00",
+      },
+      {
+        ID: "FA",
+        interID: "00",
+        date: "October 31, 2023",
+        time: "00.00",
+      },
+      {
+        ID: "QA",
+        interID: "00",
+        date: "October 31, 2023",
+        time: "00.00",
+      },
+    ],
+  },
+];
 
 export default function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug;
-  const foundInterview = interview.find((interviewee) => interviewee.ID.toString() === slug);
+  const foundInterview = interview.find(
+    (interviewee) => interviewee.ID.toString() === slug,
+  );
 
   if (foundInterview) {
     const element = foundInterview;
     const eligibleTeams = foundInterview.type.map((interviewType) => {
-      const team = teams.find((team) => team.ID === interviewType.typeID);
-      return team
+      const team = teams.find((team) => team.ID === interviewType.ID);
+      return team;
     });
     return (
       <>
-      <h1 className={`text-black text-[100px] text-center`}>🥰</h1>
-        <h1 className={`text-black text-4xl pb-16  text-center`}>Congratulations {element.name}!<br/>You are eligible to be interviewed in the following team(s):</h1>
+        <h1 className={`text-black text-[100px] text-center`}>🥰</h1>
+        <h1 className={`text-black text-4xl pb-16  text-center`}>
+          Congratulations {element.name}!<br />
+          You are eligible to be interviewed in the following team(s):
+        </h1>
 
         <div className="grid grid-flow-col auto-cols-ma gap-100 lg:px-96 p-6">
-        <SimpleGrid columns={[1, 1, 3]} spacingX='40px' spacingY='20px'>
-        {foundInterview.type.map((qE, index) => (
-            <div className='bg-black pt-6 px-6 text-white' key={index}>
-              <Image src={`${eligibleTeams[index]?.imgSrc}`} width={1000} height={1000} alt='banner' className='h-1/3'/>
-                <h1 className={`${eligibleTeams[index]?.color} text-3xl text-center pt-6 `}>{eligibleTeams[index]?.name}</h1>
+          <SimpleGrid columns={[1, 1, 3]} spacingX="40px" spacingY="20px">
+            {foundInterview.type.map((qE, index) => (
+              <div className="bg-black pt-6 px-6 text-white" key={index}>
+                <Image
+                  src={`${eligibleTeams[index]?.imgSrc}`}
+                  width={1000}
+                  height={1000}
+                  alt="banner"
+                  className="h-1/3"
+                />
+                <h1
+                  className={`${eligibleTeams[index]?.color} text-3xl text-center pt-6 `}
+                >
+                  {eligibleTeams[index]?.name}
+                </h1>
                 <h1 className="text-xl pt-6">ID: {element.ID}</h1>
                 <h1 className="text-xl pb-12">Name: {element.name}</h1>
                 {/* <h1 className='text-[#FFFFFF] text-xl text-center'>Interview ID:</h1>
                 <h1 className={`${eligibleTeams[index]?.color} text-[100px] text-center`}>{qE.typeID}{qE.interID}</h1> */}
                 {/* <h1 className='text-[#FFFFFF] text-xl text-left'>Date: {qE.date.toString()}</h1>
                 <h1 className='text-[#FFFFFF] text-xl text-left'>Round: {qE.time}</h1> */}
-            </div>
-        ))}
-      </SimpleGrid>
-
-      </div>
-      <Center>
-      <Link href={element.form} className=" px-2 py-3 bg-black text-white text-xl border-2 border-black hover:bg-white hover:text-black hover:italic">
-                Confirm to Interview
-      </Link>
-      </Center>
-
+              </div>
+            ))}
+          </SimpleGrid>
+        </div>
+        <Center>
+          <Link
+            href={element.form}
+            className=" px-2 py-3 bg-black text-white text-xl border-2 border-black hover:bg-white hover:text-black hover:italic"
+          >
+            Confirm to Interview
+          </Link>
+        </Center>
       </>
     );
   } else {
-    return <NotFound/>;
+    return (
+      <>
+        <NotFound />
+      </>
+    );
   }
 }
 
 function NotFound() {
   return (
     <>
-    <h1 className={`text-black text-[100px] text-center`}>🥹</h1>
-        <h1 className={`text-black text-4xl pb-24  text-center`}>We are sorry to hear that <br/>You are not eligible to interview.</h1>
-
+      <h1 className={`text-black text-[100px] text-center`}>🥹</h1>
+      <h1 className={`text-black text-4xl pb-24  text-center`}>
+        We are sorry to hear that <br />
+        You are not eligible to interview.
+      </h1>
     </>
   );
 }
-
-
-  const interview = [
-    {
-      ID: '6488a',
-      name: 'Thanapat Nonpassopon',
-      form: 'https://calendly.com/muictdevclub/muict-dev-club-member-interview-group-i-clone',
-      type: {
-        [
-          typeID: 'DA',
-          interID: '00',
-          date: 'October 31, 2023',
-          time: '00.00'
-        ],
-        [
-          typeID: 'FA',
-          interID: '00',
-          date: 'October 31, 2023',
-          time: '00.00'
-        ],
-        [
-          typeID: 'QA',
-          interID: '00',
-          date: 'October 31, 2023',
-          time: '00.00'
-        ],
-      }
-    },
-    ]
