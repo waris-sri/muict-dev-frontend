782fc176c6a18ea63ed7bce50f1ebef72815a1cc
Waris Sripatoomrak <waris.sri@student.mahidol.edu>
2025-04-08 00:31:50 +0700

    Update providers.tsx

--- waris-sri-portfolio/src/app/providers.tsx
+++ waris-sri-portfolio/src/app/providers.tsx
@@ -1,21 +1,9 @@
-// "use client";
-// import { ChakraProvider } from "@chakra-ui/react";
-// export function Providers({ children }: { children: React.ReactNode }) {
-//   return <ChakraProvider>{children}</ChakraProvider>;
-// }
 "use client";
 
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-// import { CacheProvider } from "@chakra-ui/next-js";
 
-const theme = extendTheme({
-  // Your theme customization here
-});
+const theme = extendTheme({});
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    // <CacheProvider>
-    <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    // </CacheProvider>
-  );
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
